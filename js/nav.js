a1734ab62fb84873a9c0344af88342585d3c68d7
Zeeshan211 <sherk3344@gmail.com>
2026-05-11 10:59:51 +0500

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -272,3 +272,14 @@
     init();
   }
 })();
+/* Load Sovereign icon decorator globally */
+(function () {
+  "use strict";
+
+  if (document.querySelector('script[src*="/js/sf-icons.js"]')) return;
+
+  const script = document.createElement("script");
+  script.src = "/js/sf-icons.js?v=1.0.0";
+  script.defer = true;
+  document.head.appendChild(script);
+})();
